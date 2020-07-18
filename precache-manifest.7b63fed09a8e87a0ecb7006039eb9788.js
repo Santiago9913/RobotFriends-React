872bcb183641475a64ed15ff3012dd2a0f6b323f
Santiago Fajardo <s.fajardot@uniandes.edu.co>
2020-07-18 16:20:29 -0500

    Updates

---
+++ precache-manifest.7b63fed09a8e87a0ecb7006039eb9788.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33345bcddcf58188ff68f1e620718fd",
+    "url": "/RobotFriends-React/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/RobotFriends-React/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "66ba38409670779a94a8",
+    "url": "/RobotFriends-React/static/css/main.af0c0eef.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/RobotFriends-React/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RobotFriends-React/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ba38409670779a94a8",
+    "url": "/RobotFriends-React/static/js/main.7503d963.chunk.js"
+  },
+  {
+    "revision": "92252555cb35d5d22978",
+    "url": "/RobotFriends-React/static/js/runtime-main.9da3746a.js"
+  }
+]);
